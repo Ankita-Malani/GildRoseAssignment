5e6af9f53f1dc99116950904ce3aedf1de85ec16
ankita.malani <ankita.malani@accenture.com>
2025-06-11 17:20:01 +0300

    <JIRA_ID>: Unnecessary folders of other language removed

--- Java/src/main/java/com/gildedrose/factory/ItemUpdaterFactory.java
+++ Java/src/main/java/com/gildedrose/factory/ItemUpdaterFactory.java
@@ -3,15 +3,20 @@ package com.gildedrose.factory;
 import com.gildedrose.constants.ItemConstants;
 import com.gildedrose.model.Item;
 import com.gildedrose.service.*;
+import com.gildedrose.constants.ItemExceptionCodeConstants;
 
 public class ItemUpdaterFactory {
 
     public static ItemUpdateService getUpdater(Item item) {
-        return switch (item.name) {
-            case ItemConstants.AGED_BRIE_ITEM_NAME -> new AgedBrieUpdateServiceImpl();
-            case ItemConstants.BACKSTAGE_ITEM_NAME -> new BackstagePassUpdateServiceImpl();
-            case ItemConstants.SULFURAS_ITEM_NAME -> new SulfurasUpdateServiceImpl();
-            default -> new DefaultUpdateServiceImpl();
-        };
+        if(item != null) {
+            return switch (item.name) {
+                case ItemConstants.AGED_BRIE_ITEM_NAME -> new AgedBrieUpdateServiceImpl();
+                case ItemConstants.BACKSTAGE_ITEM_NAME -> new BackstagePassUpdateServiceImpl();
+                case ItemConstants.SULFURAS_ITEM_NAME -> new SulfurasUpdateServiceImpl();
+                default -> new DefaultUpdateServiceImpl();
+            };
+        } else {
+            throw new NullPointerException(ItemExceptionCodeConstants.ITEM_NOT_NULL_EXCEPTION);
+        }
     }
 }
